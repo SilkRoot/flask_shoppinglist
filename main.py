4dd051b5bf6c08d752134a96c61bd7847bfb3027
lemstra <sebastian.lemstra@disy.net>
2021-04-03 21:19:19 +0200

    update

--- main.py
+++ main.py
@@ -12,146 +12,142 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
 db = SQLAlchemy(app)
 
 
-#class Element(db.Model):
-#    id = db.Column(db.Integer, primary_key=True)
-#    element = db.Column(db.String(1000))
+class FoodType(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    food_type = db.Column(db.String(1000))
 
-#    def __init__(self, id, element):
-#        self.id = id
-#        self.element = element
+    def __init__(self, id, food_type):
+        self.id = id
+        self.food_type = food_type
 
 
-class Item(db.Model):
+class FoodItem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     item = db.Column(db.String(1000))
-    type = db.Column(db.String(1000))
+    food_type_id = db.Column(db.Integer)
 
-    def __init__(self, id, item, type):
+    def __init__(self, id, item, food_type_id):
         self.id = id
         self.item = item
-        self.type = type
+        self.food_type_id = food_type_id
 
-class Shoppinglist(db.Model):
+
+class ShoppingList(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     item = db.Column(db.String(1000))
     amount = db.Column(db.Integer)
-    item_ref = db.Column(db.Integer)
+    comment = db.Column(db.String(1000))
+    food_item_id = db.Column(db.Integer)
 
-    def __init__(self, item, amount, item_ref):
+    def __init__(self, item, amount, comment, food_item_id):
         self.item = item
         self.amount = amount
-        self.item_ref = item_ref
+        self.comment = comment
+        self.food_item_id = food_item_id
 
 
 foo = [
-    Item(1001, "Apfel", "Obst"),
-    Item(1002, "Avocado", "Obst"),
-    Item(1003, "Banane", "Obst"),
-    Item(1004, "Birne", "Obst"),
-    Item(2001, "Lauch", "Gemüse"),
-    Item(2002, "Mais", "Gemüse"),
-    Item(2003, "Karotte", "Gemüse"),
-    Item(2004, "Sellerie", "Gemüse"),
-    Item(3001, "Brot", "Gebäck"),
-    Item(3002, "Croissant", "Gebäck"),
-    Item(3003, "Waffeln", "Gebäck"),
-    Item(3004, "Brötchen", "Gebäck"),
-    Item(4001, "Frischmilch", "Molkereiprodukte"),
-    Item(4001, "Brie", "Molkereiprodukte"),
-    Item(4001, "Käse", "Molkereiprodukte"),
-    Item(4001, "Butter", "Molkereiprodukte"),
-    Item(5001, "Lachs", "Fleisch und Fisch"),
-    Item(5002, "Bratwurst", "Fleisch und Fisch"),
-    Item(5003, "Hering", "Fleisch und Fisch"),
-    Item(5004, "Bacon", "Fleisch und Fisch"),
-    Item(6001, "Nori", "Zutaten und Gewürze"),
-    Item(6002, "Pfeffer", "Zutaten und Gewürze"),
-    Item(6003, "Salz", "Zutaten und Gewürze"),
-    Item(6004, "Rosmarin", "Zutaten und Gewürze"),
-    Item(7001, "Bandnudeln", "Getreideprodukte"),
-    Item(7002, "Lasagneplatten", "Getreideprodukte"),
-    Item(7003, "Weizenmehl", "Getreideprodukte"),
-    Item(7004, "Tofu", "Getreideprodukte"),
+    FoodItem(1001, "Apfel", 1),
+    FoodItem(1002, "Avocado", 1),
+    FoodItem(1003, "Banane", 1),
+    FoodItem(1004, "Birne", 1),
+    FoodItem(2001, "Lauch", 2),
+    FoodItem(2002, "Mais", 2),
+    FoodItem(2003, "Karotte", 2),
+    FoodItem(2004, "Sellerie", 2),
+    FoodItem(2005, "Brokkoli", 2),
+    FoodItem(2006, "Champignons", 2),
+    FoodItem(2007, "Cherrytomaten", 2),
+    FoodItem(2008, "Gurke", 2),
+    FoodItem(2009, "Kürbis", 2),
+    FoodItem(2010, "Grapefruit", 2),
+    FoodItem(3001, "Brot", 3),
+    FoodItem(3002, "Croissant", 3),
+    FoodItem(3003, "Waffeln", 3),
+    FoodItem(3004, "Brötchen", 3),
+    FoodItem(4001, "Frischmilch", 4),
+    FoodItem(4001, "Brie", 4),
+    FoodItem(4001, "Käse", 4),
+    FoodItem(4001, "Butter", 4),
+    FoodItem(5001, "Lachs", 5),
+    FoodItem(5002, "Bratwurst", 5),
+    FoodItem(5003, "Hering", 5),
+    FoodItem(5004, "Bacon", 5),
+    FoodItem(6001, "Nori", 6),
+    FoodItem(6002, "Pfeffer", 6),
+    FoodItem(6003, "Salz", 6),
+    FoodItem(6004, "Rosmarin", 6),
+    FoodItem(7001, "Bandnudeln", 7),
+    FoodItem(7002, "Lasagneplatten", 7),
+    FoodItem(7003, "Weizenmehl", 7),
+    FoodItem(7004, "Tofu", 7),
+]
+
+bar = [
+    FoodType(1, "Obst"),
+    FoodType(2, "Gemüse"),
+    FoodType(3, "Gebäck"),
+    FoodType(4, "Molkereiprodukte"),
+    FoodType(5, "Fleisch und Fisch"),
+    FoodType(6, "Zutaten und Gewürze"),
+    FoodType(7, "Getreideprodukte")
 ]
 
 for i in foo:
     print(i)
-    if Item.query.filter_by(id=i.id).first() is None:
+    if FoodItem.query.filter_by(id=i.id).first() is None:
         db.session.add(i)
 db.session.commit()
 
-#    date = db.Column(db.DateTime(timezone=True), default=func.now())
-#    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+for i in bar:
+    print(i)
+    if FoodType.query.filter_by(id=i.id).first() is None:
+        db.session.add(i)
+db.session.commit()
 
 @app.route("/")
 def home():
-    #	return "Hello! This is the home page <h1>HELLO</h1>"
-    items = ["Apfel", "Birne", "Banane"]
-    return render_template("index.html", items=items)
-
-
-@app.route("/test")
-def test():
-    return render_template("test.html", name="test")
-
-
-@app.route("/form", methods=["POST", "GET"])
-def form():
-    if request.method == "POST":
-        user = request.form["nm"]
-        print(user)
-        return redirect(url_for("test"))
-    else:
-        return render_template("form.html")
-
-
-#@app.route("/write_db", methods=["POST", "GET"])
-#def write_db():
-#    elements = []
-#    if request.method == "POST":
-#        # write to database
-#        new_element = Element(element=request.form["element"], id=request.form["id"])
-#        db.session.add(new_element)
-#        db.session.commit()
-#        elements = Element.query.filter_by().all()
-#        print(elements)
-#        return render_template("write_db.html", elements=elements)
-#    else:
-#        test = Element.query.filter_by().all()
-#        for i in test:
-#            print(i.element)
-#        elements = Element.query.filter_by().all()
-#        return render_template("write_db.html", elements=elements)
+    return render_template("index.html")
 
 @app.route("/add_food", methods=["POST", "GET"])
 def add_food():
-    shoppinglist = Shoppinglist.query.filter_by().all()
+    all_foodtype = FoodType.query.filter_by().all()
+    all_fooditem = FoodItem.query.filter_by().all()
     if request.method == "POST":
-        print(request.args.get('hidden_element'))
-        print(request.form)
-        print(request.get_json())
-        if 'submit_button_shoppinglist' in request.form:
-            print("pressed button from Shoppinglist and deleting it: " + request.form['submit_button_shoppinglist'])
-            Shoppinglist.query.filter_by(item=request.form['submit_button_shoppinglist']).delete()
-            db.session.commit()
+        pressed_id = (list(request.form.keys()))[0]
+        pressed_value = (list(request.form.values()))[0]
+        print(f"ID of pressed button: {pressed_id}")
+        print(f"Text of the pressed button: {pressed_value}")
+        if pressed_id is not None:
+            if ShoppingList.query.filter_by(food_item_id=pressed_id).first() \
+                    or ShoppingList.query.filter_by(item=pressed_value).first():
+                print("Element found in shoppinglist, pop it from shoppinglist")
+                ShoppingList.query.filter_by(food_item_id=pressed_id).delete()
+                ShoppingList.query.filter_by(item=pressed_value).delete()
+                db.session.commit()
+            else:
+                print("Element was NOT found in shoppinglist, add it to shoppinglist")
+                if FoodItem.query.filter_by(id=pressed_id).first():
+                    food_id = pressed_id
+                else:
+                    food_id = None
+                shoppinglist_element = ShoppingList(pressed_value, None, None, food_id)
+                db.session.add(shoppinglist_element)
+                db.session.commit()
 
-        if 'submit_button_itemlist' in request.form:
-            print("pressed button from Itemlist: " + request.form['submit_button_itemlist'])
-            shoppinglist_element = Shoppinglist(request.form['submit_button_itemlist'], 1, 1)
-            db.session.add(shoppinglist_element)
-            db.session.commit()
-        items = Item.query.filter_by().all()
-        shoppinglist = Shoppinglist.query.filter_by().all()
-        return render_template("add_food.html", items=items, shoppinglist=shoppinglist)
+        cur_shoppinglist = ShoppingList.query.filter_by().all()
+        return render_template("add_food.html",
+                               all_foodtype=all_foodtype,
+                               all_fooditem=all_fooditem,
+                               cur_shoppinglist=cur_shoppinglist)
     else:
-        items = Item.query.filter_by().all()
-        shoppinglist = Shoppinglist.query.filter_by().all()
-        return render_template("add_food.html", items=items, shoppinglist=shoppinglist)
-
-
-
+        cur_shoppinglist = ShoppingList.query.filter_by().all()
+        return render_template("add_food.html",
+                               all_foodtype=all_foodtype,
+                               all_fooditem=all_fooditem,
+                               cur_shoppinglist=cur_shoppinglist)
 
 if __name__ == "__main__":
-    #db.create_all()
+    db.create_all()
     app.run(debug=True)
 # app.run()
